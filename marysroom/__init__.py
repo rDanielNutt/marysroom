34f40a5239d9635d440f3f472f9dea7145a1bc7c
rDanielNutt <rdnutt3@gmail.com>
2023-01-06 15:43:11 -0500

    fixing import errors

--- marysroom/__init__.py
+++ marysroom/__init__.py
@@ -1,7 +1,3 @@
-from marysroom.components import activation_funcs
-from marysroom.components import optimizers
-from marysroom.components import loss_funcs
-from marysroom.components import layers
-
 import marysroom.mlfuncs
 import marysroom.models
+import marysroom.components
